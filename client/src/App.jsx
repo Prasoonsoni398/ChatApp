02a06a964a7dbea848a3efa070e329edd17b5fef
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-08-11 19:20:37 +0530

    chore: initialize project dependencies and add initial client assets

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-primary py-2 px-6 text-primary-content text-xl font-bold'>Guftagu</div>
+  )
+}
+
+export default Navbar
